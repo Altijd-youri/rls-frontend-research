79f4c100fc3c9699e9073f27bb6894ce711db85d
Yassine <31729723+Lexe003@users.noreply.github.com>
2020-05-25 14:49:11 +0200

    New design voor zegels (#6)

--- src/page/traindetails/innerbar/composition/draggable/Item.js
+++ src/page/traindetails/innerbar/composition/draggable/Item.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import { Draggable } from 'react-beautiful-dnd';
+import { Wagon, Traction } from '../../../../../utils/icons/composition';
 
 export default function Item({ item, index }) {
     const grid = 8;
     const getItemStyle = (isDragging, draggableStyle) => ({
-        // some basic styles to make the items look a bit nicer
         userSelect: 'none',
-        padding: grid * 2,
+        paddingLeft: grid * 2,
+        paddingRight: grid * 2,
         margin: `0 ${grid}px 0 0`,
-
-        // change background colour if dragging
-        background: isDragging ? 'lightgreen' : 'grey',
-
-        // styles we need to apply on draggables
+        background: isDragging ? 'lightgreen' : 'white',
+        height: "100px",
         ...draggableStyle,
     });
 
-    return (<>
+    const Icon = () => {
+        const { wagon } = item
+        return wagon ? Wagon(wagon.code) : Traction()
+    }
+
+    const getIdentifier = () => {
+        const { wagon, traction } = item
+        return wagon ? wagon.numberFreight : traction.locoNumber
+    }
+
+    const getWeight = () => {
+        const { wagon, traction } = item
+        const weight = Boolean(wagon) ? wagon.weightEmpty : traction.weight
+        return (<span>{`${weight} T`}</span>)
+    }
+
+    return (
         <Draggable key={item.id} draggableId={String(index)} index={index}>
             {(provided, snapshot) => (
                 <div
+                    className="d-flex flex-column"
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
@@ -28,9 +43,26 @@ export default function Item({ item, index }) {
                         provided.draggableProps.style
                     )}
                 >
-                    Pos: {item.position}
+                    {/* Header: Identifier */}
+                    <div className="d-flex">
+                        <span>{getIdentifier()}</span>
+                        <span style={{ marginLeft: "10px", cursor: "pointer", color: "red" }} onClick={() => { }}>
+                            <li className="far fa-minus-square" />
+                        </span>
+                    </div>
+
+                    {/* Body: Icon/Weight */}
+                    <div className="d-flex justify-content-between align-items-center">
+                        {Icon()}
+                        {getWeight()}
+                    </div>
+
+                    {/* Footer: hasDriver/Dangerlabel */}
+                    <div className="d-flex justify-content-begin align-items-center">
+                        <span>Driver {item?.driverIndication ? "present" : "absent"}</span>
+                    </div>
                 </div>
             )}
-        </Draggable></>
+        </Draggable>
     )
 }
