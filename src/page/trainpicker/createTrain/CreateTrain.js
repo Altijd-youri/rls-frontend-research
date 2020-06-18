4426b80d8a0755e9adbe2113ab4812dfc0d27e81
Yassine <31729723+Lexe003@users.noreply.github.com>
2020-06-18 11:20:04 +0200

    Change to dutch notation (#15)

--- src/page/trainpicker/createTrain/CreateTrain.js
+++ src/page/trainpicker/createTrain/CreateTrain.js
@@ -130,7 +130,7 @@ export default function CreateTrain({ onHide, setTrains }) {
                         timeFormat="HH:mm"
                         timeIntervals={15}
                         timeCaption="time"
-                        dateFormat="MMMM d, yyyy h:mm aa"
+                        dateFormat="d MMMM, yyyy h:mm aa"
                     />
                 </div>
 
