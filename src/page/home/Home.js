2ac74a9b542638cd234e8bf1b420e60f361b716c
Berend Wilkens <bwilkens@users.noreply.github.com>
2020-07-06 21:40:58 +0200

    Update Home.js

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -6,11 +6,6 @@ export default function Home() {
 
             <h1 className="mb-4">Welcome to RailLink Systems</h1>
             <p>Proudly made in the Netherlands</p>
-            <p>RailLinkSystems is een startup, gedreven door Johan de Lange en Berend Wilkens.</p>
-            <p>
-                Johan loopt sinds 1996 mee in de Nederlandse spoorwereld. Hij is zijn loopbaan begonnen als machinist, heeft de sector in zijn volle omvang leren kennen en een breed netwerk opgebouwd.
-                Berend is groot geworden in de informatietechnologie en momenteel als docent ICT verbonden aan de Hogeschool Utrecht. Hij heeft een even groot netwerk aan kennis op IT gebied opgebouwd.
-            </p>
         </div>
     )
 }
