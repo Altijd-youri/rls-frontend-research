9cbcf239f8a500a2b1c7ae7b89c6dd0672ae0058
Berend Wilkens <bwilkens@users.noreply.github.com>
2020-07-06 21:43:53 +0200

    Update Home.js

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="d-flex m-4 p-4 flex-column justify-content-center align-items-center">
 
             <h1 className="mb-4">Welcome to RailLink Systems</h1>
-            <p>Proudly made in the Netherlands</p>
+            <p>Proudly made in the Netherlands and inspired by Teun.</p>
         </div>
     )
 }
